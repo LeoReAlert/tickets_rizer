d70f648929fd6d5805771ed2a0220c7ce0a848c7
LeoReAlert <leonardocriacaoweb2021@gmail.com>
2025-01-23 14:41:20 -0400

    fix: ajustando rotas removendo espaços

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,9 @@ use App\Http\Controllers\TicketController;
 use App\Http\Controllers\SuporteController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
